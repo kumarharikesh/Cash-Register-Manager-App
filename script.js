c60a286617c73bbea7d1ace5edc55f97de78b3de
Harikesh Patel <43793294+kumarharikesh@users.noreply.github.com>
2022-10-13 03:44:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,75 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
+const messageTag = document.querySelector(".greeting");
+const flagTag = document.querySelector(".flag");
+const reset = document.querySelector("#clear");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkButton.addEventListener("click", function validateBillAndCashAmount() {
-  hideMessage();
-  if (billAmount.value > 0) {
-    // 12
-    if (cashGiven.value >= billAmount.value) {
-      // 2022> 12 => true
-      const amountToBeReturned = cashGiven.value - billAmount.value; // 2022 - 12 = 2010
-      calculateChange(amountToBeReturned);
+function clearNoteTable(notes) {
+  for (let i = 0; i < notes.length; i++) {      
+    notes[i].innerText = "";
+  }
+}
+
+function validateBillAndCashAmount() {
+
+  const b = Number(billAmount.value);
+  const c = Number(cashGiven.value);
+  const a = c - b;                           // a = Amount To Be Returned
+
+  if (b > 0) {
+                                            // let b=12 and c=2022
+    if (c >= b) {
+                                              // 2022 > 12 => true // 2022 - 12 = 2010
+      calculateChange(a);
+      messageTag.innerText = "Kindly visit again!!";
+      messageTag.style.color = "green";
+      flagTag.innerText = "YES";
+      flagTag.style.color = "green";
     } else {
-      showMessage("Do you wanna wash plates?");
+      clearNoteTable(noOfNotes);
+      messageTag.innerText = "Do you wanna wash plates?";
+      messageTag.style.color = "red";
+      flagTag.innerText = "NO";
+      flagTag.style.color = "red";
+
     }
   } else {
-    showMessage("Invalid Bill Amount");
+    clearNoteTable(noOfNotes);
+    messageTag.innerText = "Invalid Bill Amount";
+    messageTag.style.color = "red";
+    flagTag.innerText = "_";
+    flagTag.style.color = "red";
+
   }
-});
+}
 
 function calculateChange(amountToBeReturned) {
-  // 2010
-  // go over all the available
+                                                                // 2010
+                                                                // go over all the available
   for (let i = 0; i < availableNotes.length; i++) {
-    // no of notes need for the denomination
+                                                                    // no of notes need for the denomination
     const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-    // 2010 / 2000 = 1 || 10 / 500 = 0
+                                                                    // 2010 / 2000 = 1 || 10 / 500 = 0
 
-    // amount left after calculating the number of notes needed
+                                                                    // amount left after calculating the number of notes needed
     amountToBeReturned = amountToBeReturned % availableNotes[i];
-    // 2010 % 2000 = 10 || 10 % 500 = 10
+                                                                // 2010 % 2000 = 10 || 10 % 500 = 10
 
-    // updating the no of notes in the table for the current amount
+                                                                // updating the no of notes in the table for the current amount
     noOfNotes[i].innerText = numberOfNotes;
   }
 }
 
-function hideMessage() {
-  message.style.display = "none";
-}
+reset.addEventListener("click", function(){
+  clearNoteTable(noOfNotes);
+  messageTag.innerText = "";
+  flagTag.innerText = "";
+});
 
-function showMessage(msg) {
-  message.style.display = "block";
-  message.innerText = msg;
-}
+document.getElementById("cash-register-form").addEventListener("submit", function(event){
+    validateBillAndCashAmount();
+    event.preventDefault();
+});
